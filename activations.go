fe1642b6178b186b1b9abd817bc626f195f40d32
Jonathan Mackenzie <jonmac1@gmail.com>
2022-11-04 16:24:09 +1100

    added some more activations

---
+++ activations.go
@@ -0,0 +1,36 @@
+package nn_go
+
+import "math"
+
+type Activator func(float64) float64
+
+func ReLU(v float64) float64 {
+	return math.Max(0, v)
+}
+
+func Linear(v float64) float64 {
+	return v
+}
+
+func Sigmoid(v float64) float64 {
+	return 1. / (1. + math.Exp(-v))
+}
+
+func Softmax(v float64) float64 {
+	return 1 / math.Exp(v)
+}
+
+func Tanh(v float64) float64 {
+	return math.Tanh(v)
+}
+
+func LRelU(v float64) float64 {
+	if v < 0 {
+		return 0.01 * v
+	}
+	return v
+}
+
+func (a Activator) f(m *Matrix) {
+
+}
